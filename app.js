94dc75928c6f4329e201159d710e6e652ab7a70d
filsc <cfils23@gmail.com>
2021-05-12 23:55:17 -0400

    first

---
+++ app.js
@@ -0,0 +1,14 @@
+//Multiply
+function multiply(a, b) {
+    a * b
+}
+
+//get character from ASCII Value
+function getChar(c) {
+    return String.fromCharCode(c);
+}
+
+//Reverse List Order
+function reverseList(list) {
+    return list.reverse()
+}
